7f9abbbec235ae6d6a5bffc3899c41c21cb13ed9
Volodymyr <dovakin746@gmail.com>
2025-09-23 17:05:04 +0300

    Debug On

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "false"
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 CSRF_TRUSTED_ORIGINS = [f"https://{h}" for h in ALLOWED_HOSTS if not h.startswith("127.") and h != "localhost"]
